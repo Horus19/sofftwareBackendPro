2dd9750245e3dad2f5e766ee6ff048945d32eb48
Horacio Camacho <camacho19992012@gmail.com>
2022-03-07 21:34:14 -0500

    User Service

--- src/main/java/com/example/turismoapppro/controllers/MunicipioRestController.java
+++ src/main/java/com/example/turismoapppro/controllers/MunicipioRestController.java
@@ -3,6 +3,7 @@ package com.example.turismoapppro.controllers;
 import com.example.turismoapppro.models.entity.Municipio;
 import com.example.turismoapppro.models.services.IMunicipioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,7 +24,6 @@ public class MunicipioRestController {
     }
 
     @GetMapping("/municipios/{id}")
-    //@ResponseStatus(HttpStatus.OK)
     public ResponseEntity<?> show(@PathVariable Long id){
         Municipio municipio = municipioService.findById(id);
         Map<String, Object> response = new HashMap<>();
@@ -33,25 +33,59 @@ public class MunicipioRestController {
         }
         return new ResponseEntity<Municipio>(municipio, HttpStatus.OK);
     }
+
     @PostMapping("/municipios")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Municipio create(@RequestBody Municipio municipio){
-        return municipioService.save(municipio);
+    public ResponseEntity<?> create(@RequestBody Municipio municipio){
+        Municipio municipioNew = null;
+        Map<String, Object> response = new HashMap<>();
+        try {
+            municipioNew = municipioService.save(municipio);
+        }
+        catch (DataAccessException e){
+            response.put("mensaje","Error al realizar el insert en la base de datos");
+            response.put("ERROR", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return  new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        response.put("mensaje","El municipio ha sido creado con exito");
+        response.put("municipio", municipioNew);
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
     }
 
     @PutMapping("/municipios/{id}")
-    public Municipio update(@RequestBody Municipio municipio, @PathVariable Long id){
+    public ResponseEntity<?> update(@RequestBody Municipio municipio, @PathVariable Long id){
         Municipio municipioActual = municipioService.findById(id);
-        municipioActual.setNombre(municipio.getNombre());
-        return municipioService.save(municipioActual);
+        Municipio municipioUpdated = null;
+        Map<String, Object> response = new HashMap<>();
+        if(municipioActual == null){
+            response.put("mensaje", "Error: no se pudo editar, el municipio ID:".concat(id.toString()).concat(" no existe en la base de datos"));
+            return  new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+        try {
+            municipioActual.setNombre(municipio.getNombre());
+            municipioUpdated = municipioService.save(municipioActual);
+        }
+        catch (DataAccessException e){
+            response.put("mensaje","Error al actualizar el municipio en la base de datos");
+            response.put("ERROR", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return  new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        response.put("mensaje","El municipio ha sido actualizado con exito");
+        response.put("municipio", municipioUpdated);
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/municipios/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable long id){
-        municipioService.delete(id);
+    public ResponseEntity<?> delete(@PathVariable long id){
+        Map<String, Object> response = new HashMap<>();
+        try {
+            municipioService.delete(id);
+        } catch (DataAccessException e) {
+            response.put("mensaje", "Error al eliminar el municipio de la base de datos");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        response.put("mensaje", "El municipio ha sido eliminado con exito!");
+        return  new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
     }
-
-
 
 }
---
+++ src/main/java/com/example/turismoapppro/models/services/IUsuarioService.java
@@ -0,0 +1,7 @@
+package com.example.turismoapppro.models.services;
+
+import com.example.turismoapppro.models.entity.Usuario;
+
+public interface IUsuarioService {
+    public Usuario findByUsername(String username);
+}
