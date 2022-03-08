a7abca9993c589828d0d11246076250b0d78edea
Horacio Camacho <camacho19992012@gmail.com>
2022-03-07 22:50:11 -0500

    configuracion de autenticacion completada

--- src/main/java/com/example/turismoapppro/models/services/UsuarioService.java
+++ src/main/java/com/example/turismoapppro/models/services/UsuarioService.java
@@ -11,11 +11,12 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
+@Service
 public class UsuarioService implements IUsuarioService, UserDetailsService {
     private Logger logger = LoggerFactory.getLogger(UsuarioService.class);
 
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -87,7 +87,15 @@ insert into municipio (nombre) values 	                             ('Aguada');
 insert into municipio (nombre) values 	                             ('Bucaramanga');
 
 
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('Horus19cdv','$2a$10$C3Uln5uqnzx/GswADURJGOIdBqYrly9731fnwKDaUdBkt/M3qvtLq',1, 'Horacio', 'camacho','horus19@correo.com');
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('admin','$2a$10$RmdEsvEfhI7Rcm9f/uZXPebZVCcPC7ZXZwV51efAvMAp1rIaRAfPK',1, 'Dago', 'Doe','dagodoe@correo.com');
 
+INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
+INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
+
+INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (1, 1);
+INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2, 2);
+INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2, 1);
 
 
 
